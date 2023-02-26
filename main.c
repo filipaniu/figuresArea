8ee8c7bb3f164a6fda05b8cbeddfd7e97491405d
Filip <filipporeba967@gmail.com>
2023-02-26 18:52:34 +0100

    getCircleArea and getCubeVolume function

--- main.c
+++ main.c
@@ -8,8 +8,20 @@ double getTrapezeArea(double a, double b, double h) {
     return (a + b) * h / 2.0;
 }
 
+double getCubeVolume(double a) {
+    return a * a * a;
+}
+
+double getCircleArea(double r) {
+    long double pi = 22.0l / 7.0l;
+    printf("%.25Lf", pi);
+    return pi * r * r;
+}
+
 int main() {
     //  printf("%lf\n", getTriangleArea(5, 20));
-    printf("\n%lf", getTrapezeArea(3, 6, 2));
+    // printf("\n%lf", getTrapezeArea(3, 6, 2));
+    // printf("%lf", getCubeVolume(3));
+    printf("\n%.25lf", getCircleArea(12));
     return 0;
 }
