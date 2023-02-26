3587330ac886c7e93071c4f791c98e8ab564e0cc
Filip <filipporeba967@gmail.com>
2023-02-26 18:29:50 +0100

    getTrapezeArea function

--- main.c
+++ main.c
@@ -4,7 +4,12 @@ double getTriangleArea(double a, double h) {
     return (a * h) / 2.0;
 }
 
+double getTrapezeArea(double a, double b, double h) {
+    return (a + b) * h / 2.0;
+}
+
 int main() {
-    printf("%lf", getTriangleArea(5, 20));
+    //  printf("%lf\n", getTriangleArea(5, 20));
+    printf("\n%lf", getTrapezeArea(3, 6, 2));
     return 0;
 }
